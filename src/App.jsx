27cf5e46e4944bb9411ee06be42ad1448a1778d9
Karthikeya K <karthikkondavatri@gmail.com>
2024-11-29 22:32:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-
 import AdminLogin from './pages/AdminLogin';
 import ManagerLogin from './pages/ManagerLogin';
 import SalesLogin from './pages/SalesLogin';
 import AgentLogin from './pages/AgentLogin';
 //import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 const App = () => {
   return (
-   <>
-   <AdminLogin/>
-   <ManagerLogin />
-   <SalesLogin/>
-   <AgentLogin/>
-  </>
+  <Router>
+    <Routes>
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/manager" element={<ManagerLogin />} />
+      <Route path="/sales" element={<SalesLogin />} />
+      <Route path="/agent" element={<AgentLogin />} />
+    </Routes>
+  </Router>
   )
 };
 
